e0d2916725253bbb26112d4840d013fc8f1e3791
kaleeswaran <kalesmca@gmail.com>
2023-03-21 22:14:39 +0530

    basic flex design

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,21 @@ import './App.css';
 
 function App() {
   return (
-    <div>My First App for Design</div>
+    <div>My First App for Design
+
+      <div className='card'>
+        <div className='pair'>
+          <span>Name</span>
+          <span>Kaleeswaran</span>
+        </div>
+        <div className='pair'> 
+          <span>Age</span>
+          <span>29</span>
+        </div>
+        
+      </div>
+
+    </div>
   );
 }
 
